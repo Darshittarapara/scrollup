d0caef546102a7df8800fd2b6988e9221a8c543c
Darshittarapara <94553274+Darshittarapara@users.noreply.github.com>
2022-02-13 22:20:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+let btn =document.getElementById("two");
+let btn1 =document.getElementById("one");
+let btn3 =document.getElementById("three");
+let btn4 =document.getElementById("four");
+btn.addEventListener("click" ,abc);
+
+btn3.addEventListener("click" ,pqr);
+btn4.addEventListener("click",mnq)
+function abc(){
+   window.scrollBy(0,500)
+}
+function pqr(){
+   window.scrollBy(0,500)
+}
+function mnr(){
+   window.scrollBy(0,500)
+}
